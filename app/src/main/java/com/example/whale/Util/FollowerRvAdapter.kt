1468a30ca883303615c86a8a15d685c087c51976
someday55 <kgu010@naver.com>
2021-01-21 20:56:11 +0900

    210121

--- app/src/main/java/com/example/whale/Util/FollowerRvAdapter.kt
+++ app/src/main/java/com/example/whale/Util/FollowerRvAdapter.kt
@@ -1,17 +1,20 @@
 package com.example.whale.Adapters
 
 import android.content.Context
+import android.system.Os.remove
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.whale.App
 import com.example.whale.R
 import com.example.whale.ThingsTodo
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_profile_info.*
 import java.lang.Integer.parseInt
 
 class FollowerRvAdapter(val context: Context, val questList: ArrayList<ThingsTodo>) :
@@ -26,45 +29,78 @@ class FollowerRvAdapter(val context: Context, val questList: ArrayList<ThingsTod
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
+        var questList2 = arrayListOf<String>()
+        var pointList2 = arrayListOf<Int>()
+        var todoList2 = arrayListOf<ThingsTodo>()
+        var c = ""
+
         holder.bind(questList[position], context)
         holder.layoutQuest?.setOnClickListener{
-            var c = ""
+            fun search2(string: String)
+            {
+                FirebaseFirestore.getInstance()
+                    .collection("users")
+                    .whereEqualTo("id", string)
+                    .addSnapshotListener()
+                    { querySnapshot, firebaseFireStoreException ->
+                        var map: Map<String, Any> =
+                            querySnapshot?.documents?.first()?.data as Map<String, Any>
+                        questList2 = map["questList"] as ArrayList<String>
+                        pointList2 = map["pointList"] as ArrayList<Int>
+
+                    }
+            }
 
             when (App.who) {
                 1 -> {
                     c = App.follower_1[1]
-
+                    search2(App.follower_1[1])
                 }
                 2 -> {
                     c = App.follower_2[1]
-
+                    search2(App.follower_2[1])
                 }
                 3 -> {
                     c = App.follower_3[1]
-
+                    search2(App.follower_3[1])
                 }
                 4 -> {
                     c = App.follower_4[1]
-
+                    search2(App.follower_4[1])
                 }
                 5 -> {
                     c = App.follower_5[1]
-
+                    search2(App.follower_5[1])
                 }
                 6 -> {
                     c = App.follower_6[1]
-
+                    search2(App.follower_6[1])
                 }
             }
+
+            var haha = pointList2.indices
+            var baba : Int
+            for(s in haha)
+            {
+                if(questList2[s] == holder.quest1?.text.toString()){
+                    questList2.removeAt(s)
+                    //여기부터 leader가 추가하면 해당 follower의 questList로 추가되는 코드!!!!
+                }
+                if(pointList2[s].toString() == holder.point1?.text.toString()){
+                    pointList2.removeAt(s)
+                    //해당 퀘스트의 포인트 추가되는 코드
+                }
+            }
+
             var updatePoint3  = 0
             var updatePoint2  = 0
-           // App.total_quest_= App.total_quest_ - 1
+            // App.total_quest_= App.total_quest_ - 1
             if(parseInt(holder.point1?.text.toString()) != 0) {
                 App.finish_quest_= App.finish_quest_ + 1
                 holder.ingquest?.text =App.total_quest_.toString()
-
                 App.total_point =  App.total_point +parseInt(holder.point1?.text.toString())
-                }
+            }
+
 
 
             FirebaseFirestore.getInstance()
@@ -72,6 +108,15 @@ class FollowerRvAdapter(val context: Context, val questList: ArrayList<ThingsTod
                 .document(c)
                 .update("point", App.total_point)
 
+            FirebaseFirestore.getInstance()
+                .collection("users")
+                .document(c)
+                .update("questList", questList2)
+
+            FirebaseFirestore.getInstance()
+                .collection("users")
+                .document(c)
+                .update("pointList", pointList2)
 
             FirebaseFirestore.getInstance()
                 .collection("users")
@@ -82,7 +127,7 @@ class FollowerRvAdapter(val context: Context, val questList: ArrayList<ThingsTod
                 .collection("users")
                 .document(c).update("totalQuest", App.total_quest_)
 
-            var a = holder.point1?.text.toString()
+            //var a = holder.point1?.text.toString()
             var b = holder.quest1?.text.toString()
 //            var point = 0
 //            point += parseInt(a)
